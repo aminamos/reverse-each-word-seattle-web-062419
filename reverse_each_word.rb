722e06ebbeb182341e03050e6866d9ccee2f1d80
Amin Amos <aminamosahs@gmail.com>
2019-06-18 23:02:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(arg)
-  # newarr = []
-  # arg.split(" ").each do |x|
-  #  newarr << x.reverse!
-  #end
-  # newarr.join(" ")
+  newarr = []
+  arg.split(" ").each do |x|
+    newarr << x.reverse!
+  end
+  newarr.join(" ")
   
   arg.split(" ").collect {|i| i.reverse}.join(" ")
 
